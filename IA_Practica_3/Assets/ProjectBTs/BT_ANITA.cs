b0f647efcf4b670f596977ffe3b8c75d67cf304e
Helnyr <badia.noguero@gmail.com>
2022-03-30 22:19:30 +0200

    .

---
+++ IA_Practica_3/Assets/ProjectBTs/BT_ANITA.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using BTs;
+
+[CreateAssetMenu(fileName = "BT_ANITA", menuName = "Behaviour Trees/BT_ANITA", order = 1)]
+public class BT_ANITA : BehaviourTree
+{
+        
+    public override void OnConstruction()
+    {
+        /* Write here (method OnConstruction) the code that constructs the Behaviour Tree 
+           Remember to set the root attribute to a proper node
+           e.g.
+            ...
+            root = new SEQUENCE();
+            ...
+
+          A behaviour tree can use other behaviour trees.  
+      */
+    }
+}
